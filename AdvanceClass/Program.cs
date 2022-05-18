1ed14a0a24eba6155a1783962b5a738ca5bc0cbd
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-05-18 23:31:34 +0600

    Add project files.

---
+++ AdvanceClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceClass
+{
+    class Animal 
+    { 
+        string name = "Doggy";
+
+        int age = 7;
+        public void printDetails()
+        {
+            Console.WriteLine("Your Pet name is : " + name + " And Age is : " + age);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.printDetails();
+            Console.ReadLine();
+        }
+    }
+}

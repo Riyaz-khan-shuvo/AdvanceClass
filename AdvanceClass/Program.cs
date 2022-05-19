6ae185e9899e2208c3a7b11983b3db82c60c564e
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-05-19 09:48:45 +0600

    Construcstor complete

--- AdvanceClass/Program.cs
+++ AdvanceClass/Program.cs
@@ -8,21 +8,42 @@ namespace AdvanceClass
 {
     class Animal 
     { 
-        string name = "Doggy";
-
-        int age = 7;
-        public void printDetails()
+        string name ;
+        int age ;
+        string voice;
+        public static int numberOfAnimal = 0;
+        public void PrintDetails()
         {
-            Console.WriteLine("Your Pet name is : " + name + " And Age is : " + age);
+            Console.WriteLine("Your Pet name is : " + name + " Age is : " + age + " Voice is : " + voice);
+            Console.WriteLine();
         }
+        public Animal(string _name , int _age , string _voice)
+        {
+            name = _name ;
+            age = _age ;
+            voice = _voice ;
+            numberOfAnimal++;
+        }
+
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Animal animal = new Animal();
-            animal.printDetails();
+            Animal dog = new Animal("Dog" , 7 , "Gaw Gaw");
+            dog.PrintDetails();
+
+            Animal cat = new Animal("Cat", 9, "Mew mew");
+            cat.PrintDetails();
+
+            Console.WriteLine(dog.ToString());
+
+            Console.WriteLine("Total Animal : " + Animal.numberOfAnimal);
+
+            // number of animal is statice value so we can access this directly we have not to create the object in static method
+
+
             Console.ReadLine();
         }
     }
